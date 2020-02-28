c61d186f5c6c62089467a8673ec08c0d87e0b71d
Cory Williams <cory.williams@transunion.com>
2020-02-27 19:24:54 -0800

    init

---
+++ app.js
@@ -0,0 +1,23 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded());
+
+app.use('/add-product', (req, res, next) => {
+	res.send(
+		'<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>'
+	);
+});
+
+app.use('/product', (req, res) => {
+	console.log(req.body);
+	res.redirect('/');
+});
+
+app.use('/', (req, res, next) => {
+	res.send('<h1>Hello!</h1>');
+});
+
+app.listen(3000);
